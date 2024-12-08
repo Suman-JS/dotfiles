802d1cdf4b2fe75958734fd88443c960d4e2db53
Suman Mondal <sm7135120@gmail.com>
2024-12-08 23:04:20 +0530

    feat: added customization

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,48 +1,39 @@
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
   "andweeb/presence.nvim",
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require("lsp_signature").setup() end,
   },
-
-  -- == Examples of Overriding Plugins ==
-
-  -- customize alpha options
   {
     "goolord/alpha-nvim",
     opts = function(_, opts)
-      -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+        "███████╗██╗   ██╗███╗   ███╗ █████╗ ███╗   ██╗     ██╗███████╗",
+        "██╔════╝██║   ██║████╗ ████║██╔══██╗████╗  ██║     ██║██╔════╝",
+        "███████╗██║   ██║██╔████╔██║███████║██╔██╗ ██║     ██║███████╗",
+        "╚════██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║██   ██║╚════██║",
+        "███████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║╚█████╔╝███████║",
+        "╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚════╝ ╚══════╝",
+        "                                                               ",
       }
+      opts.section.header.opts = {
+        hl = "Purple",
+        position = "center",
+      }
+
+      if vim.fn.hlexists "Purple" == 0 then vim.cmd [[highlight Purple guifg=#9370DB]] end
       return opts
     end,
   },
 
-  -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = true },
 
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
@@ -51,30 +42,18 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
+      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts)
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
       local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
+      npairs.add_rules({
+        Rule("$", "$", { "tex", "latex" })
+          :with_pair(cond.not_after_regex "%%")
+          :with_pair(cond.not_before_regex("xxx", 3))
+          :with_move(cond.none())
+          :with_del(cond.not_after_regex "xx")
+          :with_cr(cond.none()),
+      }, Rule("a", "a", "-vim"))
     end,
   },
   {
@@ -130,10 +109,8 @@ return {
   {
     "onsails/lspkind.nvim",
     opts = function(_, opts)
-      -- use codicons preset
       opts.preset = "codicons"
       opts.mode = "symbol_text"
-      -- set some missing symbol types
       opts.symbol_map = {
         Array = "",
         Boolean = "",
